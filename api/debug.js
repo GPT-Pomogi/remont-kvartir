b3b1ca27bdd088605542de55a3d46cb76b50c901
GPT-Pomogi <yasprosilai-cell@users.noreply.github.com>
2026-06-25 00:28:57 +0300

    Remove temporary debug endpoint (no longer needed, leaked env var presence)

--- api/debug.js
+++
@@ -1,8 +0,0 @@
-module.exports = function handler(req, res) {
-  res.json({
-    kv_url:   !!process.env.KV_REST_API_URL,
-    kv_token: !!process.env.KV_REST_API_TOKEN,
-    tg_token: !!process.env.TG_TOKEN,
-    tg_chat:  !!process.env.TG_CHAT_ID,
-  });
-};
